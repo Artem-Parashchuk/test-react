bf64fa676f8e4cf67e34a0356918d821e06b1437
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-04-29 20:08:47 +0300

    finished counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,19 +1,55 @@
-import s from './Counter.module.css'
+import { useState } from "react";
+import s from "./Counter.module.css";
+
 export const Counter = () => {
+  const [count, setCount] = useState(1);
+  const [step, setStep] = useState(1);
+
+  const incrementCount = () => {
+    setCount((prev) => prev + step);
+  };
+
+  const decrementCount = () => {
+    setCount((prev) => prev - step);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+    setStep(1);
+  };
   return (
     <div className={s.wrapper}>
-        <div className={s.content}>
-            <p className={s.count}>
-10
-            </p>
-            <input type="number" className={s.input}/>
+      <div className={s.content}>
+        <h1 className={s.count}>{count}</h1>
 
-            <div className={s.btn_block}>
-                <button className={s.btn}>Minus</button>
-                <button className={s.btn}>Reset</button>
-                <button className={s.btn}>Plus</button>
-            </div>
+        <input
+          type="number"
+          className={s.input}
+          onChange={(e) => setStep(+e.target.value)}
+          value={step}
+        />
+
+        <div className={s.btn_block}>
+          <button
+            className={s.btn}
+            onClick={decrementCount}
+          >
+            Minus
+          </button>
+          <button
+            className={s.btn}
+            onClick={resetCount}
+          >
+            Reset
+          </button>
+          <button
+            className={s.btn}
+            onClick={incrementCount}
+          >
+            Plus
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
